90b5fe1c0a1450ac0faa9a3b89f109cda45c82cc
Ryan Whitney <ryan.whitney@me.com>
2020-11-25 02:52:22 -0800

    rainbow

--- dotter/neatcommands.js
+++ dotter/neatcommands.js
@@ -16,3 +16,21 @@ function moreRainbow(){
   }
   moreRainbow();
 };
+
+const moreRainbowCode = "
+function moreRainbow(){
+    for(i=0;i<16;i++){
+    let color = "hsl("+ i*20 +",65%,70%)";
+    vertical(i, color);
+  };
+  setTimeout(function(){
+    for(i=0;i<16;i++){
+      let color = "hsl("+ i*20 +",65%,70%)";
+      horizontal(i, color);
+    }
+    setTimeout(function(){
+      moreRainbow();
+    },500);
+  },500);
+}
+moreRainbow();";
