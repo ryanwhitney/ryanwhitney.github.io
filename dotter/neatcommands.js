aa2d60140fba20cf88c549ce8f0990aa1b321b77
Ryan Whitney <ryan.whitney@me.com>
2020-11-25 00:22:55 -0800

    More rainbow

---
+++ dotter/neatcommands.js
@@ -0,0 +1,18 @@
+function moreRainbow(){
+  function moreRainbow(){
+     for(i=0;i<16;i++){
+      let color = "hsl("+ i*20 +",65%,70%)";
+      vertical(i, color);
+    };
+    setTimeout(function(){
+      for(i=0;i<16;i++){
+       let color = "hsl("+ i*20 +",65%,70%)";
+       horizontal(i, color);
+      }
+      setTimeout(function(){
+          moreRainbow();
+        },500);
+    },500);
+  }
+  moreRainbow();
+};
